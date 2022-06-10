9de7b17bfcd271d153fbd8ecd83bc99f98491988
Gaurav Dogra <gauru.dogra@gmail.com>
2022-06-10 07:10:36 +0100

    Added Comment.cs

---
+++ MeetupClone/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupClone.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public Member Member { get; set; }
+    
+    [Required]
+    public string Text { get; set; }
+}
