7ecae2a543d1cbafe96c9a2f8723212a4e73be10
Gaurav Dogra <gauru.dogra@gmail.com>
2022-06-10 05:52:37 +0100

    created Meeting class

---
+++ MeetupClone/Models/Meeting.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupClone.Models;
+
+public class Meeting
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MinLength(2)]
+    public string Name { get; set; }
+
+    [Required]
+    [MinLength(32)]
+    public string About { get; set; }
+
+    [Required]
+    public Boolean IsOnline { get; set; }
+
+    [Required]
+    public DateTime DateTime { get; set; }
+
+    public List<Member> Hosting { get; set; }
+    public List<Member> Going { get; set; }
+}
